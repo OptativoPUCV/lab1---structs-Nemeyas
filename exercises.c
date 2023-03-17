1fd08789b641f4813a9ae5eaac19517cff6435e4
Replit user <>
2023-03-17 04:03:04 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -61,7 +61,7 @@ Persona* crearPersona(char nombre[], char rut[], int edad) {
     return NULL;
   }
    //asignar valores de entrada a los campos de p
-  strncpy(p->nombre, nombre[30]);
+  strncpy(p->nombre, nombre,30);
   strncpy(p->rut, rut);
     p->edad=edad;
   
