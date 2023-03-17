f3dd9bbc9cd7aa81e7888df9ce3cc6631041261a
Replit user <>
2023-03-17 04:23:19 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -127,5 +127,15 @@ Use las operaciones implementadas de vectores para
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
-
+  Vector* a = crearVector(a1, a2);
+  Vector* b = crearVector(b1, b2);
+  if (a == NULL || b == NULL) {
+    printf("Error: no se pudieron crear los vectores a y b\n");
+    return;
+  }
+  c->x = a->x + b->x;
+  c->y = a->y + b->y;
+  
+  destruirVector(a);
+  destruirVector(b);
 }
